f6b129059ac121c03f3d09f1e5b44ba2e2816619
artisancommons <artisancommons@gmail.com>
2023-05-31 13:53:06 -0400

    initial ecs

--- core/ecs/ecs.cpp
+++ core/ecs/ecs.cpp
@@ -10,3 +10,38 @@ void Test_mod_ecs_init() {
     PrintD("Test::ecs_init")
 }
 
+
+namespace MAGE {
+namespace ECS {
+
+void Scene::Init(int bytes) {
+    this->arena = LibFn(new_arena)(bytes);
+}
+void Scene::Destroy() {
+    LibFn(arena_dispose)(&this->arena);
+}
+
+void SceneManager::Start() {
+    if (this->scenes.find("main_menu") != this->scenes.end())
+        this->scenes["main_menu"]->Play(this);
+    else
+        printf("-  Fatal:: Failed to Start Scene Player, No Entry 'main_menu'\n");
+}
+
+void SceneManager::Play(std::string name) {
+    if (this->scenes.find(name) != this->scenes.end())
+        this->scenes[name]->Play(this);
+    else
+        printf("- Fatal:: SceneManager could not Play Scene<%s>(Unavailable)\n", name.c_str());
+}
+
+void SceneManager::Destroy() {
+    for (auto& scene : this->scenes) {
+        scene.second->Destroy();
+        free(scene.second);
+    }
+}
+
+}
+}
+
--- core/ecs/include.hpp
+++ core/ecs/include.hpp
@@ -6,6 +6,94 @@ extern "C" {
     #include "lib.h"
 }
 
-//
+
+#include <unordered_map>
+#include <string>
+
+#include <memory/include.hpp>
+
+namespace MAGE {
+namespace ECS {
+
+class Scene {
+public:
+    template <class T>
+    Memory::ArenaVar<T> CreateObject(T obj_copy) {
+        int size = sizeof(T);
+        l64 sp = LibFn(arena_push)(&this->arena, size, &obj_copy);
+        
+        if (Backend_FailArena(sp)) 
+            return (void*)0;
+
+        void* loc = &this->arena.arr[sp];
+        printf("- ECS::Scene::CreateObject : (bytes):%lu, (ArenaAddr):%p, (SP): %lu\n", size, loc, sp);
+
+        return Memory::ArenaVar<T>(loc);
+    }
+
+    void Init(int bytes);
+    void Destroy();
+
+protected:
+    cArena arena;
+};
+
+//class IScenePlayer {
+//public:
+ //   virtual void Play(SceneManager* sceneManager) {}
+//};
+
+class ISceneLoader {//}: public IScenePlayer {
+public:
+    virtual void InitScene() {}
+    virtual void LoadScene() {}
+    virtual void Play(void* sceneManager) {}
+    virtual void Destroy  () {}
+
+protected:
+    Scene scene;
+};
+class SceneManager {
+public:
+    SceneManager() {}
+
+    template <class T>
+    T* NewScene(std::string name) {
+        if (this->scenes.find(name) != this->scenes.end()) return 0;
+
+        printf("- NewScene(%s) \n", name.c_str());
+
+        T* scene_loader = new T();// (T*)malloc(sizeof(T*));
+        scene_loader->InitScene();
+        this->scenes.emplace(name, scene_loader);
+
+        return scene_loader;
+    }
+
+    void Start();
+    void Play(std::string name);
+
+    void Destroy();
+
+private:
+    std::unordered_map<std::string, ISceneLoader*> scenes;
+};
+
+
+
+
+class SystemPassthroughModifier {
+public:
+    // is registered into a system
+    // the system interally call registered modifiers
+    // this can be used for system manipulation without modifiying the source code
+
+private:
+
+};
+
+
+
+}}
 
 #endif // !MOD_ECS
