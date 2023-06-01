d78836b3cf3b206a8ad95e38cddb41b49f6adab7
artisancommons <artisancommons@gmail.com>
2023-05-31 20:58:13 -0400

    added logging

--- core/_common/sdebug.h
+++ core/_common/sdebug.h
@@ -2,8 +2,23 @@
 #ifndef COMMON_SDEBUG
 #define COMMON_SDEBUG
 
+
 #include <stdio.h>
 
 #define PrintD(msg) printf("- #%d :: %s\n", __LINE__, msg);
+
+
+#ifdef FLAG_TESTMODE
+    #include <iostream>
+    #include <fstream>
+
+    #define Log(msg) PrintD(msg)
+    #define PATH_LOG_FILE "log/output.txt"
+    #define LogFile(msg) { Log("- logging to file...") ofstream f; f.open(PATH_LOG_FILE); f << msg << "\n"; f.close(); }
+#else
+    #define Log(msg) /// disabled due to non-test mode
+    #define LogFile(msg) /// disabled due to non-test mode
+#endif
+
 
 #endif
